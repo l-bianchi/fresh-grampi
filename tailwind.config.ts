de86e3eca48165f141237477a23c379a534ee6eb
l-bianchi <illorenzobianchi@proton.me>
2024-05-22 16:32:23 +0200

    Try adding catppuccin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import { type Config } from "tailwindcss";
+import { flavors } from "https://deno.land/x/catppuccin/mod.ts";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    extend: { ...flavors.mocha.colors },
+  },
 } satisfies Config;
