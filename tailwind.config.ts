7095c6fda0245799a8abf350574749ee821599f7
l-bianchi <illorenzobianchi@proton.me>
2024-05-22 17:22:48 +0200

    Starting to get mad

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
 import { type Config } from "tailwindcss";
 import { flavorEntries } from "https://deno.land/x/catppuccin@v1.2.0/mod.ts";
 
+const colors = Object.fromEntries(
+  flavorEntries.flatMap(([_, flavor]) =>
+    flavor.colorEntries.map((
+      [colorName, { hex }],
+    ) => [`${flavor.name}-${colorName}`, hex])
+  ),
+);
+console.log(colors);
+
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
-      colors: Object.fromEntries(
-        flavorEntries.flatMap(([_, flavor]) =>
-          flavor.colorEntries.map((
-            [colorName, { hex }],
-          ) => [`${flavor.name}-${colorName}`, hex])
-        ),
-      ),
+      colors,
     },
   },
 } satisfies Config;
