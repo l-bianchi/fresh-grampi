55857594a62378c0d66aec7694d067e841b46e52
l-bianchi <illorenzobianchi@proton.me>
2024-05-22 16:56:55 +0200

    Try logging all colors

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,9 +1,16 @@
 import type { Signal } from "@preact/signals";
 import { Button } from "../components/Button.tsx";
+import { flavorEntries } from "https://deno.land/x/catppuccin@v1.2.0/mod.ts";
 
 interface CounterProps {
   count: Signal<number>;
 }
+
+flavorEntries.map(([_, flavor]) => {
+  flavor.colorEntries.map(([colorName, { hex }]) => {
+    console.log(colorName, hex);
+  });
+});
 
 export default function Counter(props: CounterProps) {
   return (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
 import { type Config } from "tailwindcss";
-import { flavors } from "https://deno.land/x/catppuccin@v1.2.0/mod.ts";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
-  theme: { extend: { "mauve": flavors.mocha.colors.mauve.hex } },
 } satisfies Config;
