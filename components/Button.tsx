f2fc6e51a1331fa53b1319f1a30c7958f4964644
l-bianchi <illorenzobianchi@proton.me>
2024-05-22 16:38:10 +0200

    Show catppuccin mocha colors

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,7 +6,7 @@ export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border-pink border-2 rounded bg-white hover:bg-rosewater transition-colors"
+      class="px-2 py-1 border-test border-2 rounded bg-white hover:bg-rosewater transition-colors"
     />
   );
 }
--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,5 +1,6 @@
 import type { Signal } from "@preact/signals";
 import { Button } from "../components/Button.tsx";
+import { flavors } from "https://deno.land/x/catppuccin/mod.ts";
 
 interface CounterProps {
   count: Signal<number>;
@@ -11,6 +12,7 @@ export default function Counter(props: CounterProps) {
       <Button onClick={() => props.count.value -= 1}>-1</Button>
       <p class="text-3xl tabular-nums">{props.count}</p>
       <Button onClick={() => props.count.value += 1}>+1</Button>
+      {flavors.mocha.colors}
     </div>
   );
 }
