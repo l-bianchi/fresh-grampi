5e77126467d0bc857073c3cae973bf0871682080
l-bianchi <illorenzobianchi@proton.me>
2024-05-24 16:22:18 +0200

    Fix the style of generator

--- routes/[sessionId].tsx
+++ routes/[sessionId].tsx
@@ -2,10 +2,10 @@ import { PageProps } from "$fresh/server.ts";
 
 export default function Greet({ params }: PageProps) {
   return (
-    <div class="size-full bg-latte-mantle dark:bg-mocha-mantle border-2 border-latte-mauve dark:border-mocha-mauve rounded items-center justify-center">
+    <div class="size-screen p-10 bg-latte-mantle dark:bg-mocha-mantle border-2 border-latte-mauve dark:border-mocha-mauve rounded items-center justify-center">
       <h1 class="text-4xl font-semibold text-latte-text dark:text-mocha-text">
         You are in the room
-        <span class="font-bold text-latte-mauve dark:text-mocha-mauve">
+        <span class="font-bold text-latte-mauve dark:text-mocha-mauve underline ml-4">
           {params.sessionId}
         </span>
       </h1>
