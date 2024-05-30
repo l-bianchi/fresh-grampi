aa1a98cd58cefb32815e6b0bdcdc1fb8537b8d05
l-bianchi <illorenzobianchi@proton.me>
2024-05-30 21:38:08 +0200

    Fix issue with height

--- routes/[sessionId].tsx
+++ routes/[sessionId].tsx
@@ -6,7 +6,7 @@ export default function Room({ params }: PageProps) {
   return (
     <div class="w-full h-full p-4">
       <div class="flex w-full h-full gap-2">
-        <div class="w-3/4">
+        <div class="w-3/4 h-full">
           <Editor />
         </div>
         <div class="w-1/4 h-full bg-mocha-base rounded shadow-xl overflow-hidden">
