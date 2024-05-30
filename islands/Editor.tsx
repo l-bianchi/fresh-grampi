7c8e96f263d0149d3f500ec6488a1167820a8e34
l-bianchi <illorenzobianchi@proton.me>
2024-05-30 20:26:01 +0200

    Bigger shadows

--- islands/Editor.tsx
+++ islands/Editor.tsx
@@ -4,10 +4,10 @@ import { Prompt } from "../components/Prompt.tsx";
 export default function Editor() {
   return (
     <div class="flex flex-col h-full w-full gap-2">
-      <div class="flex h-full bg-mocha-base border-2 border-mocha-surface0 rounded shadow-md items-center justify-center">
+      <div class="flex h-full bg-mocha-base border-2 border-mocha-surface0 rounded shadow-xl items-center justify-center">
         <ImagePreview />
       </div>
-      <div class="h-fit bg-mocha-base border-2 border-mocha-surface0 rounded shadow-md p-4">
+      <div class="h-fit bg-mocha-base border-2 border-mocha-surface0 rounded shadow-xl p-4">
         <Prompt />
       </div>
     </div>
