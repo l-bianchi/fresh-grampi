fbceedbaa39fbca5347b06b6c1ce3e2452d00d4b
l-bianchi <illorenzobianchi@proton.me>
2024-05-30 20:20:03 +0200

    Edit chat bubble style

--- components/ChatBubble.tsx
+++ components/ChatBubble.tsx
@@ -1,18 +1,20 @@
 export function ChatBubble() {
   return (
     <div class="flex flex-col w-full leading-relaxed p-4 border-mocha-lavender bg-mocha-surface0 rounded shadow">
-      <div class="flex items-center space-x-2">
+      <div class="flex justify-between">
+        <div class="flex items-center space-x-2">
+          <span class="text-sm font-semibold text-mocha-text">
+            Bonnie Green
+          </span>
+          <span class="text-sm font-normal text-mocha-subtext0">
+            11:46
+          </span>
+        </div>
         <img
-          class="flex w-8 h-8 rounded-full"
+          class="w-8 h-8 rounded-full"
           src="https://placehold.co/128x128"
           alt="Jese image"
         />
-        <span class="text-sm font-semibold text-mocha-text">
-          Bonnie Green
-        </span>
-        <span class="text-sm font-normal text-mocha-subtext0">
-          11:46
-        </span>
       </div>
       <p class="text-sm font-normal py-2.5 text-mocha-text">
         That's awesome. I think our users will really appreciate the
