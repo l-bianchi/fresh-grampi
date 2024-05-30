b78e77fa44f6cd704fa03a351024ea11d1a73da6
l-bianchi <illorenzobianchi@proton.me>
2024-05-30 20:22:47 +0200

    Trying scroll

--- islands/Conversations.tsx
+++ islands/Conversations.tsx
@@ -2,7 +2,18 @@ import { ChatBubble } from "../components/ChatBubble.tsx";
 
 export default function Conversations() {
   return (
-    <div class="flex flex-col p-4 gap-2 items-center">
+    <div class="flex flex-col p-4 gap-2 items-center overflow-y-auto">
+      <ChatBubble />
+      <ChatBubble />
+      <ChatBubble />
+      <ChatBubble />
+      <ChatBubble />
+      <ChatBubble />
+      <ChatBubble />
+      <ChatBubble />
+      <ChatBubble />
+      <ChatBubble />
+      <ChatBubble />
       <ChatBubble />
       <ChatBubble />
     </div>
