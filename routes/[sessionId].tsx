10e2dd075e5c7f67706a26cd25a5652e1208af9f
l-bianchi <illorenzobianchi@proton.me>
2024-05-30 21:40:20 +0200

    Fix issue with height

--- routes/[sessionId].tsx
+++ routes/[sessionId].tsx
@@ -5,11 +5,11 @@ import Conversations from "../islands/Conversations.tsx";
 export default function Room({ params }: PageProps) {
   return (
     <div class="w-full h-full p-4">
-      <div class="flex w-full h-full gap-2">
-        <div class="w-3/4 h-full">
+      <div class="flex gap-2">
+        <div class="w-3/4">
           <Editor />
         </div>
-        <div class="w-1/4 h-full bg-mocha-base rounded shadow-xl overflow-hidden">
+        <div class="w-1/4 bg-mocha-base rounded shadow-xl overflow-hidden">
           <Conversations sessionId={params.sessionId} />
         </div>
       </div>
