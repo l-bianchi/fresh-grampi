11dd426e25998cbbf5c04c6fab7a533bc6bde924
l-bianchi <illorenzobianchi@proton.me>
2024-05-30 19:55:25 +0200

    Edit chat bubble style

--- components/ChatBubble.tsx
+++ components/ChatBubble.tsx
@@ -1,28 +1,23 @@
 export function ChatBubble() {
   return (
-    <div class="flex items-start gap-2">
-      <img
-        class="w-8 h-8 rounded-full"
-        src="https://placehold.co/128x128"
-        alt="Jese image"
-      />
-      <div class="flex flex-col w-full max-w-[320px] leading-relaxed p-4 border-mocha-lavender bg-mocha-surface0 rounded-e-xl rounded-es-xl">
-        <div class="flex items-center space-x-2">
-          <span class="text-sm font-semibold text-mocha-text">
-            Bonnie Green
-          </span>
-          <span class="text-sm font-normal text-mocha-subtext0">
-            11:46
-          </span>
-        </div>
-        <p class="text-sm font-normal py-2.5 text-mocha-text">
-          That's awesome. I think our users will really appreciate the
-          improvements.
-        </p>
-        <span class="text-sm font-normal text-mocha-subtext0">
-          Delivered
+    <div class="flex flex-col w-full max-w-[320px] leading-relaxed p-4 border-mocha-lavender bg-mocha-surface0 rounded-e-xl rounded-es-xl">
+      <div class="flex items-center space-x-2">
+        <span class="text-sm font-semibold text-mocha-text">
+          Bonnie Green
         </span>
+        <span class="text-sm font-normal text-mocha-subtext0">
+          11:46
+        </span>
+        <img
+          class="w-8 h-8 rounded-full self-end"
+          src="https://placehold.co/128x128"
+          alt="Jese image"
+        />
       </div>
+      <p class="text-sm font-normal py-2.5 text-mocha-text">
+        That's awesome. I think our users will really appreciate the
+        improvements.
+      </p>
     </div>
   );
 }
