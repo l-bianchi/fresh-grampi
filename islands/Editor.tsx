676dc454eaaa02377e3093faeb5585c50a66359b
l-bianchi <illorenzobianchi@proton.me>
2024-05-30 20:51:49 +0200

    Try other image preview setup

--- islands/Editor.tsx
+++ islands/Editor.tsx
@@ -6,6 +6,11 @@ export default function Editor() {
     <div class="flex flex-col h-full w-full gap-2">
       <div class="flex h-full bg-mocha-base rounded shadow-xl items-center justify-center">
         <ImagePreview />
+        <img
+          class="h-auto max-w-xl rounded-lg shadow-xl dark:shadow-gray-800"
+          src="/logo.svg"
+          alt="image description"
+        />
       </div>
       <div class="h-fit bg-mocha-base rounded shadow-xl p-4">
         <Prompt />
