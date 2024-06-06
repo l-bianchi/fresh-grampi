f6885eefae49db28ed89d12bf22f5e8a0fb8bd47
l-bianchi <illorenzobianchi@proton.me>
2024-06-06 15:57:43 +0200

    Use correct envs

--- routes/api/supabase.ts
+++ routes/api/supabase.ts
@@ -1,22 +1,21 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.43.4";
 import { FreshContext } from "$fresh/server.ts";
-//import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
-import { load } from "$std/dotenv/mod.ts";
 
 export const handler = async (
   _req: Request,
   _ctx: FreshContext,
 ): Promise<Response> => {
-  const env = await load();
-  console.log(env);
+  const supabaseUrl = Deno.env.get("SUPABASE_URL");
+  const supabaseAnonPublic = Deno.env.get("SUPABASE_ANON_PUBLIC");
 
-  const supabase = createClient(
-    env["SUPABASE_URL"],
-    env["SUPABASE_ANON_PUBLIC"],
-  );
+  if (supabaseUrl && supabaseAnonPublic) {
+    const supabase = createClient(supabaseUrl, supabaseAnonPublic);
 
-  const { data } = await supabase
-    .from("messages")
-    .select();
-  return new Response(JSON.stringify(data));
+    const { data } = await supabase
+      .from("messages")
+      .select();
+    return new Response(JSON.stringify(data));
+  }
+
+  return new Response("Error connecting to supabase!");
 };
