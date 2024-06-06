8cc1d69a763ef22fd0833e0accd6f927e177e048
l-bianchi <illorenzobianchi@proton.me>
2024-06-06 17:51:52 +0200

    Try using service role key

--- routes/api/messages/index.ts
+++ routes/api/messages/index.ts
@@ -4,7 +4,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.43.4";
 export const handler: Handlers = {
   async POST(_req: Request, _ctx: FreshContext): Promise<Response> {
     const supabaseUrl = Deno.env.get("SUPABASE_URL");
-    const supabaseAnonPublic = Deno.env.get("SUPABASE_ANON_PUBLIC");
+    const supabaseAnonPublic = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
 
     if (supabaseUrl && supabaseAnonPublic) {
       const supabase = createClient(supabaseUrl, supabaseAnonPublic);
@@ -25,7 +25,7 @@ export const handler: Handlers = {
   },
   async GET(_req: Request, _ctx: FreshContext): Promise<Response> {
     const supabaseUrl = Deno.env.get("SUPABASE_URL");
-    const supabaseAnonPublic = Deno.env.get("SUPABASE_ANON_PUBLIC");
+    const supabaseAnonPublic = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
 
     if (supabaseUrl && supabaseAnonPublic) {
       const supabase = createClient(supabaseUrl, supabaseAnonPublic);
