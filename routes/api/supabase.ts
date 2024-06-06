6e67dfc3efb4e80ff99b6bb03f4cdb903586375d
l-bianchi <illorenzobianchi@proton.me>
2024-06-06 15:46:20 +0200

    Test deploy action

--- routes/api/supabase.ts
+++ routes/api/supabase.ts
@@ -2,17 +2,17 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.43.4";
 import { FreshContext } from "$fresh/server.ts";
 import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
-const env = await load();
-
-const supabase = createClient(
-  env["SUPABASE_URL"],
-  env["SUPABASE_ANON_PUBLIC"],
-);
-
 export const handler = async (
   _req: Request,
   _ctx: FreshContext,
 ): Promise<Response> => {
+  const env = await load();
+
+  const supabase = createClient(
+    env["SUPABASE_URL"],
+    env["SUPABASE_ANON_PUBLIC"],
+  );
+
   const { data } = await supabase
     .from("messages")
     .select();
