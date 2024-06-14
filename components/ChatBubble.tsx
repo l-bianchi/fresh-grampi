0cf33592095ccd37da8a3715540a917b9e87662b
l-bianchi <illorenzobianchi@proton.me>
2024-06-14 17:10:35 +0200

    Fix dates

--- components/ChatBubble.tsx
+++ components/ChatBubble.tsx
@@ -1,24 +1,17 @@
 interface ChatBubbleProps {
   text: string;
-  created_at: Date;
+  created_at: string;
 }
 export function ChatBubble({ text, created_at }: ChatBubbleProps) {
   return (
     <div class="flex flex-col w-full leading-relaxed p-4 border-mocha-lavender bg-mocha-surface0 rounded shadow">
-      <div class="flex justify-between">
-        <div class="flex items-center space-x-2">
-          <span class="text-sm font-semibold text-mocha-text">
-            Anonymous
-          </span>
-          <span class="text-sm font-normal text-mocha-subtext0">
-            {created_at}
-          </span>
-        </div>
-        <img
-          class="w-8 h-8 rounded-full"
-          src="https://placehold.co/128x128"
-          alt="Jese image"
-        />
+      <div class="flex items-center space-x-2">
+        <span class="text-sm font-semibold text-mocha-text">
+          Anonymous
+        </span>
+        <span class="text-sm font-normal text-mocha-subtext0">
+          {new Date(created_at).toLocaleTimeString()}
+        </span>
       </div>
       <p class="text-sm font-normal py-2.5 text-mocha-text">
         {text}
--- stores/messages.ts
+++ stores/messages.ts
@@ -4,20 +4,20 @@ import {
 } from "https://esm.sh/statery@0.5.4?alias=react:preact/compat&deps=preact@10.19.6";
 
 const store = makeStore<
-  { messages: { text: string; created_at: Date; sessionId: string }[] }
+  { messages: { text: string; created_at: string; sessionId: string }[] }
 >({
   messages: [],
 });
 
 export const setMessages = (
-  messages: { text: string; created_at: Date; sessionId: string }[],
+  messages: { text: string; created_at: string; sessionId: string }[],
 ) =>
   store.set((_state) => ({
     messages: messages,
   }));
 
 export const addMessage = (
-  message: { text: string; created_at: Date; sessionId: string },
+  message: { text: string; created_at: string; sessionId: string },
 ) =>
   store.set((state) => ({
     messages: [...state.messages, message],
